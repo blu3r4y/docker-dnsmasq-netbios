4aa3f21b9992f4104e31a357f514f34bb6c6b8ef
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-07-17 02:24:02 +0200

    Print an error if the configuration volume isn't mapped or empty

--- run.sh
+++ run.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-echo "Starting netbios scanner in the background ..."
-nohup ./netbios_scanner.sh &
+CONF_DIR="/etc/dnsmasq"
 
-echo "Starting dnsmasq in the foreground ..."
-exec dnsmasq -k
+if [ -d "$CONF_DIR" ]
+then
+    if [ "$(ls -A $CONF_DIR)" ]; then
+
+        echo -n "found configuration files in $CONF_DIR: "
+        ls -A $CONF_DIR
+
+        echo "starting netbios scanner in the background ..."
+        nohup ./netbios_scanner.sh >/dev/null 2>&1 &
+
+        echo "starting dnsmasq in the foreground ..."
+        exec dnsmasq -k
+
+    else
+        echo "error: configuration directory $CONF_DIR is empty - did you map a volume?"
+    fi
+else
+    echo "error: configuration directory $CONF_DIR is missing - did you map a volume?"
+fi
+
+exit 1
